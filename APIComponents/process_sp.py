1db266bc2a43f6823730ee558751fea5fc7854a2
Mustafizur Rahman <sojibvikings@gmail.com>
2023-12-31 17:52:32 +0600

    last try

--- APIComponents/process_sp.py
+++ APIComponents/process_sp.py
@@ -1,22 +1,14 @@
-
 class ExecuteStoredProcedure:
     @classmethod
     def ExecuteProcedure(cls, procedure_name, params, conn):
-
         cursor = conn.cursor()
-        procedure_name = procedure_name
-        param_markers = ", ".join(["?" for _ in params])
-        sql_query = f"CALL {procedure_name} {param_markers}"
-        cursor.execute(sql_query, params)
-        data = cursor.fetchall()
         json_list = []
-        for i in range(len(data[0].cursor_description)):
-            x = {
-                data[0].cursor_description[i][0]: [column[i] for column in data]
-            }
-            json_list.append(x)
-
-        conn.commit()
+        procedure_name = procedure_name
+        if params:
+            param_markers = ", ".join(["%s" for _ in params])
+            sql_query = "CALL " + procedure_name + "(" + param_markers + ")"
+            cursor.execute(sql_query, params)
+            conn.commit()
         cursor.close()
         conn.close()
         return json_list
